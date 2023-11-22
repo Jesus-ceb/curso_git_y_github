02945c3acb2c27b840254f48954e753195918d15
jesusceballos <jesusce9413@gmail.com>
2023-11-22 15:37:41 -0500

    Se agrego el archivo main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+print("Hola a todos este es un archivo python")
+
+print("esta es una simple prueba del curso de git & GitHub")
+
+print("esta es una linea '3' ")
+
+print("esta es una linea '4' ")
+
